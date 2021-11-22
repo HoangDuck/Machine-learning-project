7db41057f8e6b8fb855e627aa4cbe2be0232141e
HoangDuck <hoanghuuduc22@gmail.com>
2021-11-22 07:19:04 +0700

    commit

--- Project/Get_Json_Year_Experiences.py
+++ Project/Get_Json_Year_Experiences.py
@@ -1,18 +1,30 @@
+#%%
 import json
 data=None
 file_path='Json_Files/YearOfExperience.json'
 # Opening JSON file
 with open(file_path) as json_file:
     data = json.load(json_file)
+print(data)
+def ClassifyData(data,text_raw_data):
+    result=''
+    keys=data.keys()
+    for key in keys:
+        index=text_raw_data.find(key)
+        if(index!=-1):
+            result+=data[key]
+    return result
+#%%
+p=ClassifyData(data,'-')
+print(p)
 
-# def ClassifyData(data,text_raw_data):
-#     result=''
-#     keys=data.keys()
-#     for key in keys:
-#         index=text_raw_data.find(key)
-#         if(index!=-1):
-#             result+=data[key]+"; "
-#     return result
+# %%
+a=1.5
+print(type(a))
+# %%
+import numpy as np
+list_Year_Experience=np.zeros
+list_Year_Experience[0]=2
+list_Year_Experience[5]=3
 
-# p=ClassifyData(data,0)
-# print(p)
+# %%
