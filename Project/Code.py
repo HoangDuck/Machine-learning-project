7bf970787af826cfe37a7563427fd30cf77881e0
HoangDuck <hoanghuuduc22@gmail.com>
2021-11-22 13:13:48 +0700

    Duc commit

--- Project/Code.py
+++ Project/Code.py
@@ -589,4 +589,281 @@ print(processed_train_set_val[[0, 1, 2],:].toarray())
 print(processed_train_set_val.shape)
 print('We have %d numeric feature + 1 added features + 32 cols of onehotvector for categorical features.' %(len(num_feat_names)))
 
-# %%
+''' WEEK 04 '''
+
+# In[5]: TRAIN AND EVALUATE MODELS 
+
+# 5.1 Try LinearRegression model
+# 5.1.1 Training: learn a linear regression hypothesis using training data 
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(processed_train_set_val, train_set_labels)
+print('\n____________________________________ LinearRegression ____________________________________')
+print('Learned parameters: ', model.coef_)
+
+# 5.1.2 Compute R2 score and root mean squared error
+def r2score_and_rmse(model, train_data, labels): 
+    r2score = model.score(train_data, labels)
+    from sklearn.metrics import mean_squared_error
+    prediction = model.predict(train_data)
+    mse = mean_squared_error(labels, prediction)
+    rmse = np.sqrt(mse)
+    return r2score, rmse      
+r2score, rmse = r2score_and_rmse(model, processed_train_set_val, train_set_labels)
+print('R2 score (on training data, best=1):', r2score)
+print("Root Mean Square Error: ", rmse.round(decimals=1))
+        
+# 5.1.3 Predict labels for some training instances
+print("Input data: \n", train_set.iloc[0:9])
+print("Predictions: ", model.predict(processed_train_set_val[0:9]).round(decimals=1))
+print("Labels:      ", list(train_set_labels[0:9]))
+
+# 5.1.4 Store models to files, to compare latter
+#from sklearn.externals import joblib 
+import joblib # new lib
+def store_model(model, model_name = ""):
+    # NOTE: sklearn.joblib faster than pickle of Python
+    # INFO: can store only ONE object in a file
+    if model_name == "": 
+        model_name = type(model).__name__
+    joblib.dump(model,'saved_objects/' + model_name + '_model.pkl')
+def load_model(model_name):
+    # Load objects into memory
+    #del model
+    model = joblib.load('saved_objects/' + model_name + '_model.pkl')
+    #print(model)
+    return model
+store_model(model)
+
+
+# 5.2 Try DecisionTreeRegressor model
+# Training
+from sklearn.tree import DecisionTreeRegressor
+model = DecisionTreeRegressor()
+model.fit(processed_train_set_val, train_set_labels)
+# Compute R2 score and root mean squared error
+print('\n____________________________________ DecisionTreeRegressor ____________________________________')
+r2score, rmse = r2score_and_rmse(model, processed_train_set_val, train_set_labels)
+print('R2 score (on training data, best=1):', r2score)
+print("Root Mean Square Error: ", rmse.round(decimals=1))
+store_model(model)
+# Predict labels for some training instances
+#print("Input data: \n", train_set.iloc[0:9])
+print("Predictions: ", model.predict(processed_train_set_val[0:9]).round(decimals=1))
+print("Labels:      ", list(train_set_labels[0:9]))
+
+
+# 5.3 Try RandomForestRegressor model
+# Training (NOTE: may take time if train_set is large)
+from sklearn.ensemble import RandomForestRegressor
+model = RandomForestRegressor(n_estimators = 5, random_state=42) # n_estimators: no. of trees
+model.fit(processed_train_set_val, train_set_labels)
+# Compute R2 score and root mean squared error
+print('\n____________________________________ RandomForestRegressor ____________________________________')
+r2score, rmse = r2score_and_rmse(model, processed_train_set_val, train_set_labels)
+print('R2 score (on training data, best=1):', r2score)
+print("Root Mean Square Error: ", rmse.round(decimals=1))
+store_model(model)      
+# Predict labels for some training instances
+#print("Input data: \n", train_set.iloc[0:9])
+print("Predictions: ", model.predict(processed_train_set_val[0:9]).round(decimals=1))
+print("Labels:      ", list(train_set_labels[0:9]))
+
+
+# 5.4 Try polinomial regression model
+# NOTE: polinomial regression can be treated as (multivariate) linear regression where high-degree features x1^2, x2^2, x1*x2... are seen as new features x3, x4, x5... 
+# hence, to do polinomial regression, we add high-degree features to the data, then call linear regression
+# 5.5.1 Training. NOTE: may take a while 
+from sklearn.preprocessing import PolynomialFeatures
+poly_feat_adder = PolynomialFeatures(degree = 2) # add high-degree features to the data
+train_set_poly_added = poly_feat_adder.fit_transform(processed_train_set_val)
+new_training = 10
+if new_training:
+    model = LinearRegression()
+    model.fit(train_set_poly_added, train_set_labels)
+    store_model(model, model_name = "PolinomialRegression")      
+else:
+    model = load_model("PolinomialRegression")
+# 5.4.2 Compute R2 score and root mean squared error
+print('\n____________________________________ Polinomial regression ____________________________________')
+r2score, rmse = r2score_and_rmse(model, train_set_poly_added, train_set_labels)
+print('R2 score (on training data, best=1):', r2score)
+print("Root Mean Square Error: ", rmse.round(decimals=1))
+# 5.4.3 Predict labels for some training instances
+print("Predictions: ", model.predict(train_set_poly_added[0:9]).round(decimals=1))
+print("Labels:      ", list(train_set_labels[0:9]))
+
+
+# 5.5 Evaluate with K-fold cross validation 
+from sklearn.model_selection import cross_val_score
+#from sklearn.model_selection import ShuffleSplit, StratifiedKFold, StratifiedShuffleSplit
+#from sklearn.model_selection import cross_val_predict
+
+#cv1 = ShuffleSplit(n_splits=10, test_size=0.2, random_state=42); 
+#cv2 = StratifiedKFold(n_splits=10, random_state=42); 
+#cv3 = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=42); 
+print('\n____________________________________ K-fold cross validation ____________________________________')
+
+run_evaluation = 0
+if run_evaluation:
+    # Evaluate LinearRegression
+    model_name = "LinearRegression" 
+    model = LinearRegression()             
+    nmse_scores = cross_val_score(model, processed_train_set_val, train_set_labels, cv=5, scoring='neg_mean_squared_error')
+    rmse_scores = np.sqrt(-nmse_scores)
+    joblib.dump(rmse_scores,'saved_objects/' + model_name + '_rmse.pkl')
+    print("LinearRegression rmse: ", rmse_scores.round(decimals=1))
+
+    # Evaluate DecisionTreeRegressor
+    model_name = "DecisionTreeRegressor" 
+    model = DecisionTreeRegressor()
+    nmse_scores = cross_val_score(model, processed_train_set_val, train_set_labels, cv=5, scoring='neg_mean_squared_error')
+    rmse_scores = np.sqrt(-nmse_scores)
+    joblib.dump(rmse_scores,'saved_objects/' + model_name + '_rmse.pkl')
+    print("DecisionTreeRegressor rmse: ", rmse_scores.round(decimals=1))
+
+    # Evaluate RandomForestRegressor
+    model_name = "RandomForestRegressor" 
+    model = RandomForestRegressor()
+    nmse_scores = cross_val_score(model, processed_train_set_val, train_set_labels, cv=5, scoring='neg_mean_squared_error')
+    rmse_scores = np.sqrt(-nmse_scores)
+    joblib.dump(rmse_scores,'saved_objects/' + model_name + '_rmse.pkl')
+    print("RandomForestRegressor rmse: ", rmse_scores.round(decimals=1))
+    
+    # Evaluate Polinomial regression
+    model_name = "PolinomialRegression" 
+    model = LinearRegression()
+    nmse_scores = cross_val_score(model, train_set_poly_added, train_set_labels, cv=5, scoring='neg_mean_squared_error')
+    rmse_scores = np.sqrt(-nmse_scores)
+    joblib.dump(rmse_scores,'saved_objects/' + model_name + '_rmse.pkl')
+    print("Polinomial regression rmse: ", rmse_scores.round(decimals=1))
+else:
+    # Load rmse
+    model_name = "LinearRegression" 
+    rmse_scores = joblib.load('saved_objects/' + model_name + '_rmse.pkl')
+    print("LinearRegression rmse: ", rmse_scores.round(decimals=1))
+    print("Avg. rmse: ", mean(rmse_scores.round(decimals=1)),'\n')
+
+    model_name = "DecisionTreeRegressor" 
+    rmse_scores = joblib.load('saved_objects/' + model_name + '_rmse.pkl')
+    print("DecisionTreeRegressor rmse: ", rmse_scores.round(decimals=1))
+    print("Avg. rmse: ", mean(rmse_scores.round(decimals=1)),'\n')
+
+    model_name = "RandomForestRegressor" 
+    rmse_scores = joblib.load('saved_objects/' + model_name + '_rmse.pkl')
+    print("RandomForestRegressor rmse: ", rmse_scores.round(decimals=1))
+    print("Avg. rmse: ", mean(rmse_scores.round(decimals=1)),'\n')
+
+    model_name = "PolinomialRegression" 
+    rmse_scores = joblib.load('saved_objects/' + model_name + '_rmse.pkl')
+    print("Polinomial regression rmse: ", rmse_scores.round(decimals=1))
+    print("Avg. rmse: ", mean(rmse_scores.round(decimals=1)),'\n')
+
+
+# In[6]: FINE-TUNE MODELS 
+# NOTE: this takes TIME
+# INFO: find best hyperparams (param set before learning, e.g., degree of polynomial in poly reg, no. of trees in rand forest, no. of layers in neural net)
+# Here we fine-tune RandomForestRegressor and PolinomialRegression
+print('\n____________________________________ Fine-tune models ____________________________________')
+def print_search_result(grid_search, model_name = ""): 
+    print("\n====== Fine-tune " + model_name +" ======")
+    print('Best hyperparameter combination: ',grid_search.best_params_)
+    print('Best rmse: ', np.sqrt(-grid_search.best_score_))  
+    print('Best estimator: ', grid_search.best_estimator_)  
+    print('Performance of hyperparameter combinations:')
+    cv_results = grid_search.cv_results_
+    for (mean_score, params) in zip(cv_results["mean_test_score"], cv_results["params"]):
+        print('rmse =', np.sqrt(-mean_score).round(decimals=1), params) 
+
+method = 2
+# 6.1 Method 1: Grid search (try all combinations of hyperparams in param_grid)
+if method == 1:
+    from sklearn.model_selection import GridSearchCV
+    
+    run_new_search = 0      
+    if run_new_search:
+        # 6.1.1 Fine-tune RandomForestRegressor
+        model = RandomForestRegressor(random_state=42)
+        param_grid = [
+            # try 12 (3×4) combinations of hyperparameters (bootstrap=True: drawing samples with replacement)
+            {'bootstrap': [True], 'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+            # then try 6 (2×3) combinations with bootstrap set as False
+            {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]} ]
+            # Train across 5 folds, hence a total of (12+6)*5=90 rounds of training 
+        grid_search = GridSearchCV(model, param_grid, cv=5, scoring='neg_mean_squared_error', return_train_score=True)
+        grid_search.fit(processed_train_set_val, train_set_labels)
+        joblib.dump(grid_search,'saved_objects/RandomForestRegressor_gridsearch.pkl')
+        print_search_result(grid_search, model_name = "RandomForestRegressor")      
+
+        # 6.1.2 Fine-tune Polinomial regression          
+        model = Pipeline([ ('poly_feat_adder', PolynomialFeatures()), # add high-degree features
+                           ('lin_reg', LinearRegression()) ]) 
+        param_grid = [
+            # try 3 values of degree
+            {'poly_feat_adder__degree': [1, 2, 3]} ] # access param of a transformer: <transformer>__<parameter> https://scikit-learn.org/stable/modules/compose.html
+            # Train across 5 folds, hence a total of 3*5=15 rounds of training 
+        grid_search = GridSearchCV(model, param_grid, cv=5, scoring='neg_mean_squared_error', return_train_score=True)
+        grid_search.fit(processed_train_set_val, train_set_labels)
+        joblib.dump(grid_search,'saved_objects/PolinomialRegression_gridsearch.pkl') 
+        print_search_result(grid_search, model_name = "PolinomialRegression") 
+    else:
+        # Load grid_search
+        grid_search = joblib.load('saved_objects/RandomForestRegressor_gridsearch.pkl')
+        print_search_result(grid_search, model_name = "RandomForestRegressor")         
+        grid_search = joblib.load('saved_objects/PolinomialRegression_gridsearch.pkl')
+        print_search_result(grid_search, model_name = "PolinomialRegression") 
+
+
+# In[7]: ANALYZE AND TEST YOUR SOLUTION
+# NOTE: solution is the best model from the previous steps. 
+
+# 7.1 Pick the best model - the SOLUTION
+# Pick Random forest
+search = joblib.load('saved_objects/RandomForestRegressor_randsearch.pkl')
+best_model = search.best_estimator_
+# Pick Linear regression
+#best_model = joblib.load('saved_objects/LinearRegression_model.pkl')
+
+print('\n____________________________________ ANALYZE AND TEST YOUR SOLUTION ____________________________________')
+print('SOLUTION: ' , best_model)
+store_model(best_model, model_name="SOLUION")   
+
+# 7.2 Analyse the SOLUTION to get more insights about the data
+# NOTE: ONLY for rand forest
+if type(best_model).__name__ == "RandomForestRegressor":
+    # Print features and importance score  (ONLY on rand forest)
+    feature_importances = best_model.feature_importances_
+    onehot_cols = []
+    for val_list in full_pipeline.transformer_list[1][1].named_steps['cat_encoder'].categories_: 
+        onehot_cols = onehot_cols + val_list.tolist()
+    feature_names = train_set.columns.tolist() + ["TỔNG SỐ PHÒNG"] + onehot_cols
+    for name in cat_feat_names:
+        feature_names.remove(name)
+    print('\nFeatures and importance score: ')
+    print(*sorted(zip( feature_names, feature_importances.round(decimals=4)), key = lambda row: row[1], reverse=True),sep='\n')
+
+# 7.3 Run on test data
+processed_test_set = full_pipeline.transform(test_set)  
+# 7.3.1 Compute R2 score and root mean squared error
+r2score, rmse = r2score_and_rmse(best_model, processed_test_set, test_set_labels)
+print('\nPerformance on test data:')
+print('R2 score (on test data, best=1):', r2score)
+print("Root Mean Square Error: ", rmse.round(decimals=1))
+# 7.3.2 Predict labels for some test instances
+print("\nTest data: \n", test_set.iloc[0:9])
+print("Predictions: ", best_model.predict(processed_test_set[0:9]).round(decimals=1))
+print("Labels:      ", list(test_set_labels[0:9]),'\n')
+
+
+'''
+Kết quả hiện chưa tốt. 
+Gợi ý cả thiện:
+1. Xóa các samples bất thường (giá quá cao, quá thấp)
+2. Xóa cột HƯỚNG, hoặc tốt nhất là thêm dữ liệu (2000+)
+'''
+
+
+# In[8]: LAUNCH, MONITOR, AND MAINTAIN YOUR SYSTEM
+# Go to slide: see notes
+
+done = 1
