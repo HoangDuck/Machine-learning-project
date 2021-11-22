86d59a7a0f35bab6601c3974d034e960220ba589
HoangDuck <hoanghuuduc22@gmail.com>
2021-11-22 07:03:00 +0700

    delete list test and add getjsonyear

---
+++ Project/Get_Json_Year_Experiences.py
@@ -0,0 +1,18 @@
+import json
+data=None
+file_path='Json_Files/YearOfExperience.json'
+# Opening JSON file
+with open(file_path) as json_file:
+    data = json.load(json_file)
+
+# def ClassifyData(data,text_raw_data):
+#     result=''
+#     keys=data.keys()
+#     for key in keys:
+#         index=text_raw_data.find(key)
+#         if(index!=-1):
+#             result+=data[key]+"; "
+#     return result
+
+# p=ClassifyData(data,0)
+# print(p)
